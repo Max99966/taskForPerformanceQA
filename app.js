03fd1377d5f9a3bf7981e0928540c60df2b2622c
Max <maksimhakobjanyan@gmail.com>
2024-06-10 01:02:43 +0400

    Finished v

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const { performance } = require('perf_hooks');
-
-app.use(bodyParser.json());
+const {performance} = require('perf_hooks');
 
 app.get('/index', (req, res) => {
     const start = performance.now();
@@ -19,7 +16,7 @@ app.get('/index', (req, res) => {
 
 app.get('/moreload', (req, res) => {
     const start = performance.now();
-    const numbers = Array.from({ length: 1000 }, () => Math.floor(Math.random() * 1000));
+    const numbers = Array.from({length: 1000}, () => Math.floor(Math.random() * 1000));
     const sumOfNumbers = numbers.reduce((acc, cur) => acc + cur, 0);
     const end = performance.now();
     const executionTime = end - start;
@@ -27,6 +24,10 @@ app.get('/moreload', (req, res) => {
         console.warn(`Warning: Execution time for /moreload exceeded limit (${executionTime} ms)`);
     }
     res.send(sumOfNumbers.toString());
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Node Express App');
 });
 
 app.listen(port, () => {
